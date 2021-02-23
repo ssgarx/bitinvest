91356efa22edd9c67b0c2f0b5c45fc8def8171a1
Sagar <sagarchandgadkar74@gmail.com>
2021-02-23 17:38:55 +0530

    Actual price now displayed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import Up from '../images/Up.png';
+import Down from '../images/Down.png';
 
 function Dashboard(props) {
 
-    console.log('Data passed form Login to Dashboard', props.location.matchedUserData)
+    var [loggedUser, setloggedUser] = useState("NoUserLogged");
+    var [btc, setbtc] = useState("0");
+    var [volChnage, setvolChnage] = useState("0");
+    var [prcChange, setprcChange] = useState("0");
+    var [mcChange, setmcChange] = useState("0");
+    var [marcUp, setmarcUp] = useState(false)
+
+    useEffect(() => {
+        console.log('useEff API fetch');
+        setloggedUser(props.location.matchedUserData.fName)
+        axios.get("https://api.nomics.com/v1/currencies/ticker?key=ecf232234b93686e9abc884ceda89756&ids=BTC,&interval=1h&convert=INR&per-page=100&page=1")
+            .then(function (response) {
+                setbtc(parseFloat(Number(response.data[0].price).toFixed(2)));
+                var { volume_change_pct, price_change_pct, market_cap_change_pct } = response.data[0]["1h"];
+                if (price_change_pct > 0) { setmarcUp(true) } else { setmarcUp(false) }
+                setvolChnage(volume_change_pct);
+                setprcChange(price_change_pct);
+                setmcChange(market_cap_change_pct);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+    //WHEN ENABLED CAN REFRESH THE PRICE AT REFRESH RATE WE CHOOSE
+    // setInterval(() => {
+    //     console.log('timer executing');
+    //     axios.get("https://api.nomics.com/v1/currencies/ticker?key=ecf232234b93686e9abc884ceda89756&ids=BTC,&interval=1h&convert=INR&per-page=100&page=1")
+    //         .then(function (response) {
+    //             // console.log(response.data[0]["1h"]);
+    //             // console.log(parseFloat(Number(response.data[0].price).toFixed(2)));
+    //             setbtc(parseFloat(Number(response.data[0].price).toFixed(2)));
+    //             setvolChnage(response.data[0]["1h"].volume_change_pct);
+    //             setprcChange(response.data[0]["1h"].price_change_pct);
+    //             setmcChange(response.data[0]["1h"].market_cap_change_pct);
+    //             // var { 1h } = response.data[0];
+    //             // console.log(1h);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         })
+    // }, 900000);
+
     return (
         <>
             <Container className="dc1">
@@ -11,15 +56,18 @@ function Dashboard(props) {
                     <Row>
                         <Col className="dc1c1">
                             <div style={{ textAlign: "left" }} className="dc1a1">
-                                <p className="dc1c1p1">Welcome back Sagar!</p>
+                                <p className="dc1c1p1">Welcome back {loggedUser}!</p>
                             </div>
                             <div style={{ textAlign: "center" }} className="dc1a2">
-                                <p>current price is <span>₹858585</span></p>
+                                <p>current price is
+                                <span>₹{btc}</span>
+                                    <span>{marcUp ? <img src={Up} alt="arrow-up" /> : <img src={Down} alt="arrow-down" />}</span>
+                                </p>
                             </div>
                             <div style={{ textAlign: "center" }} className="dc1a3">
-                                <div><p>volume change: <span>0.1409%</span></p></div>
-                                <div><p>price change: <span>-0.0320%</span></p></div>
-                                <div><p>mkt. cap. change: <span>0.0069%</span></p></div>
+                                <div><p>volume change: <span>{volChnage}%</span></p></div>
+                                <div><p>price change: <span>{prcChange}%</span></p></div>
+                                <div><p>mkt. cap. change: <span>{mcChange}%</span></p></div>
                             </div>
                             <div style={{ textAlign: "center" }} className="dc1a4">
                                 <div>
@@ -34,7 +82,6 @@ function Dashboard(props) {
                 </Container>
             </Container>
 
-
             <Container className="dc2 mt-4">
                 <Container className="dc2b">
                     <Row>
@@ -43,7 +90,7 @@ function Dashboard(props) {
                                 <p className="">Your investment</p>
                             </div>
                             <div style={{ textAlign: "center" }} className="dc2a2b">
-                                <p><span>₹12122</span></p>
+                                <p><span>₹12122</span> <span><img src={Up} alt="arrow-up" /></span></p>
                             </div>
                             <div style={{ textAlign: "left" }} className="dc2a2c">
                                 <p className="">Your wallet</p>
@@ -64,15 +111,17 @@ function Dashboard(props) {
                                 <p className="">Your orders</p>
                             </div>
                             <div style={{ textAlign: "center" }} className="dc2a1b">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                                <p>23/02/2021 ₹452 buy at ₿3294656.23</p>
+                                <p>23/02/2021 ₹522 buy at ₿3294242.25</p>
+                                <p>23/02/2021 ₹282 buy at ₿3294236.26</p>
+                                <p>23/02/2021 ₹872 buy at ₿3294762.62</p>
+                                <p>23/02/2021 ₹252 buy at ₿3294365.53</p>
+                                <p>23/02/2021 ₹225 buy at ₿3294238.52</p>
+                                <p>23/02/2021 ₹862 buy at ₿3294663.50</p>
+                                <p>23/02/2021 ₹871 buy at ₿3294725.93</p>
+                                <p>23/02/2021 ₹684 buy at ₿3294672.36</p>
+                                <p>23/02/2021 ₹284 buy at ₿3294636.52</p>
+                                <p>23/02/2021 ₹126 buy at ₿3294635.92</p>
                             </div>
                         </Col>
                     </Row>
